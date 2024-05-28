c791a592350b72054292c520e1ac5ea60cdd323b
Leo Dion <leogdion@brightdigit.com>
2024-05-28 11:06:25 -0400

    working on client

---
+++ HarvestBin/HarvestBin/Advertiser.swift
@@ -0,0 +1,79 @@
+//
+//  Advertiser.swift
+//  HarvestBin
+//
+//  Created by Leo on 5/28/24.
+//
+
+import Foundation
+import MultipeerConnectivity
+
+
+class Advertiser : NSObject, MCNearbyServiceAdvertiserDelegate, MCSessionDelegate {
+
+    
+
+
+ 
+    
+
+    
+    
+    let id : UInt64
+    let session : MCSession
+    let advertiser : MCNearbyServiceAdvertiser
+    var receiver : GuestServiceReceiver!
+    
+    override init () {
+        self.id = .random(in: 0...(.max))
+        let peerID = MCPeerID(displayName: "host")
+        session = MCSession(peer: peerID)
+        advertiser = .init(peer: peerID, discoveryInfo: nil, serviceType: "bushelgs")
+        
+        super.init()
+        
+        advertiser.delegate = self
+        session.delegate = self
+        
+    }
+    
+
+    func startWith(receiver: GuestServiceReceiver) {
+        assert(self.receiver == nil)
+        self.receiver = receiver
+        self.advertiser.startAdvertisingPeer()
+    }
+    
+    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didNotStartAdvertisingPeer error: Error) {
+        dump(error)
+    }
+    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
+        session.connectPeer(peerID, withNearbyConnectionData: .init())
+        invitationHandler(true, session)
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        
+            print("session \(session.myPeerID) with \(peerID) did start receive resource \(resourceName)")
+    }
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        
+    }
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+    }
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        self.receiver.didReceiveData(data)
+    }
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        self.receiver.connectionUpdateTo(.init(state: state, peerID: peerID))
+    }
+    
+}
+
+extension ConnectionStatus {
+    init(state: MCSessionState, peerID: MCPeerID) {
+        self.init(rawValue: state.rawValue, displayName: peerID.displayName)
+    }
+    
+}
---
+++ HarvestBin/HarvestBin/GuestServicesObject.swift
@@ -0,0 +1,71 @@
+//
+//  GuestServicesObject.swift
+//  HarvestBin
+//
+//  Created by Leo on 5/28/24.
+//
+
+import Foundation
+
+protocol GuestServiceReceiver {
+    func didReceiveData(_ data: Data)
+    func connectionUpdateTo(_ status: ConnectionStatus)
+    
+}
+enum ConnectionStatus {
+    case initialized
+    case connectingTo(String)
+    case notConnected
+    case connected(String)
+    case unknown(String)
+}
+class GuestServicesObject : ObservableObject, GuestServiceReceiver {
+    func didReceiveData(_ data: Data) {
+        dump(data)
+        guard let newMessage = String(bytes: data, encoding: .utf8) else {
+            assertionFailure("Unable to interpert data")
+            return
+        }
+        DispatchQueue.main.async {
+            self.lastMessage = newMessage
+        }
+    }
+    
+    func connectionUpdateTo(_ status: ConnectionStatus) {
+        
+        DispatchQueue.main.async {
+            dump(status)
+            self.connectedState = status
+        }
+    }
+    
+    @Published var lastMessage = ""
+    @Published var connectedState = ConnectionStatus.initialized
+    let advertiser : Advertiser
+    
+    init () {
+        self.advertiser = .init()
+    }
+    
+    func start () {
+        self.advertiser.startWith(receiver: self)
+    }
+}
+
+extension ConnectionStatus {
+    
+    init(rawValue: Int, displayName: String) {
+        assert(rawValue >= 0 && rawValue <= 2)
+        switch rawValue {
+        case 0:
+            self = .notConnected
+        case 1:
+            self = .connectingTo(displayName)
+        case 2:
+            self = .connected(displayName)
+        default:
+            
+            self = .unknown(displayName)
+        }
+    }
+}
