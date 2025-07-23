9b4b27c9ce5833d48052d7417b80693090b0589f
leogdion <leogdion@brightdigit.com>
2025-07-23 10:21:53 -0400

    setting up project

--- Packages/HarvestBinKit/Package.swift
+++ Packages/HarvestBinKit/Package.swift
@@ -5,11 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "HarvestBinKit",
+    dependencies: [
+      .package(path: "../../../BushelKit")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "HarvestBinKit"
+            name: "harvestbin",
+            dependencies: [
+              "HarvestBinKit"
+            ]
         ),
+        .target(
+            name: "HarvestBinKit",
+            dependencies: [
+              .product(name: "BushelFoundation", package: "BushelKit"),
+              .product(name: "BushelGuestProfile", package: "BushelKit"),
+            ]
+        )
     ]
 )
--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -15,6 +15,7 @@ import PackageDescription
 let package = Package(
     name: "HarvestBin",
     dependencies: [
+      .package(path: "./Packages/HarvestBinKit")
         // Add your own dependencies here:
         // .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
         // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
